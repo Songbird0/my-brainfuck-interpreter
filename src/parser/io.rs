1e5525e6a2cb1ffa16e6267a3129c33cefb0deaa
Anthony Defranceschi <chaacygg@gmail.com>
2019-04-22 19:51:15 +0200

    Move `print` and `feed` to `io.rs`

---
+++ src/parser/io.rs
@@ -0,0 +1,2 @@
+fn print(input: &[u8]) -> nom::IResult<&[u8], (), u32> { unimplemented!() }
+fn feed(input: &[u8]) -> nom::IResult<&[u8], (), u32> { unimplemented!() }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,10 +1,3 @@
 pub mod ast;
 pub mod ram;
-
-use std::fmt;
-
-use nom::{is_alphanumeric, AsBytes};
-
-
-fn print(input: &[u8]) -> nom::IResult<&[u8], (), u32> { unimplemented!() }
-fn feed(input: &[u8]) -> nom::IResult<&[u8], (), u32> { unimplemented!() }
+pub mod io;
