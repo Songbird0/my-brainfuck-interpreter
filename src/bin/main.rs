f59eaa06554f7c8f279066135be55c0940faf2f2
Anthony Defranceschi <chaacygg@gmail.com>
2019-04-23 23:55:13 +0200

    Integrate the interpreter

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,15 @@
-#[macro_use]
-extern crate nom;
-
 extern crate brainfuck_interpreter;
 
-fn main() {}
+use brainfuck_interpreter::parser::ast::Interpreter;
+
+fn main() {
+    let interpreter: Interpreter = Interpreter {
+        ram: [0; 30_000],
+        ram_ptr: 0,
+        program: "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.".as_bytes(),
+        program_ptr: 0,
+        stack: vec![]
+    };
+
+    interpreter.run();
+}
